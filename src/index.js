4cfa58730d2690efb2281f58b6ae9179d6e6fd5f
Lana Schuster <lanaschuster1@gmail.com>
2022-11-12 18:41:51 -0300

    chore: add pinia

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import { router } from './router/index';
 import App from './App.vue';
 
 const app = createApp(App);
+const pinia = createPinia();
 
 app.use(router);
+app.use(pinia);
 
 app.mount('#main');
